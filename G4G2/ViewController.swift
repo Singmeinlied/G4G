714c60d2bcb49fade98982be944d63b36f09862c
Singmeinlied <98156702+Singmeinlied@users.noreply.github.com>
2023-02-03 16:35:12 +0600

    First commit

---
+++ G4G2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  G4G2
+//
+//  Created by Abai Kush on 3/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
